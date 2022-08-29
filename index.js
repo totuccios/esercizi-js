1551ca5c35f5002e10193c487b45c629ecac5713
salvo <salvo.spina.ss@virgilio.it>
2022-08-29 08:53:27 +0200

    es 2 js

---
+++ index.js
@@ -0,0 +1,13 @@
+function canPlay() {
+    const PERSON_NAME = 'Paul';
+  
+    if (true) {
+      PERSON_NAME = 'George';
+    }
+  
+    PERSON_NAME += ' plays football';
+  
+    console.log(PERSON_NAME);
+  }
+  
+  canPlay();
