d3da093e1d7f21b35f3fe1687599fb5f485b279f
totuccios <106822007+totuccios@users.noreply.github.com>
2022-09-02 08:21:26 +0200

    es 8

---
+++ es-08/exercise-5.js
@@ -0,0 +1,25 @@
+function calculateAverageAge(persons) {
+  let avg = 0;
+
+  for(let i = 0; i < persons.length; i++){
+    avg += persons[i].age;
+  }
+  return avg / persons.length
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
