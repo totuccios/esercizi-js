b0fe6a4568bc6b4b87a20b74e9d1f1e0051cdb5e
totuccios <106822007+totuccios@users.noreply.github.com>
2022-09-02 08:19:27 +0200

    es 7

---
+++ es-07/exercise-4.js
@@ -0,0 +1,25 @@
+function nicknameMap(persons) {
+  let nickName = [];
+
+  for(let i=0; i<persons.length; i++) {
+    nickName.push(persons[i].name + "-" + persons[i].age);
+  }
+  return nickName
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
